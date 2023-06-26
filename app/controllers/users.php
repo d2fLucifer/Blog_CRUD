8915edf7c2206551e0ed0ec202b7bc61ac1a6b61
d2fLucifer <d2f.working@gmail.com>
2023-06-26 23:30:11 +0700

    fix bugs create usersimage

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,7 +1,6 @@
 <?php
 include ROOT_PATH . "/app/database/db.php";
 include ROOT_PATH . "/app/helpers/ValidateUsers.php";
-$role = '';
 
 $errors = array();
 $username = '';
@@ -29,10 +28,10 @@ function loginUser($user)
     }
 }
 
-if (isset($_POST['register-btn']) || isset($_POST['create-admin'])) {
+if (isset($_POST['register-btn'])|| isset($_POST['create-admin'])) {
     $errors = validateUser($_POST);
     if (count($errors) === 0) {
-        unset($_POST['register-btn'], $_POST['repeat-password'], $_POST['create-admin']);
+        unset($_POST['register-btn'], $_POST['repeat-password'],$_POST['create-admin']);
 
         if (!empty($_FILES['image']['name'])) {
             $image_name = time() . "_" . $_FILES['image']['name'];
@@ -50,20 +49,21 @@ if (isset($_POST['register-btn']) || isset($_POST['create-admin'])) {
 
         $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $_POST['password'] = $hashedPassword;
-
-        if ($_POST['role']) {
-            $_POST['role'] = "Admin";
-            $user_id = create($table, $_POST);
-            $_SESSION['message'] = 'Admin user created successfully';
-            $_SESSION['type'] = 'success';
-            header('location:' . BASE_URL . '/admin/users/index.php');
-            exit();
-        } else {
-            $_POST['role'] = 'Author';
-            $user_id = create($table, $_POST);
-            $user = selectOne($table, ['id' => $user_id]);
-            loginUser($user);
-        }
+if($_POST['role'])
+{
+    $_POST['role'] = "Admin";
+    $user_id = create($table, $_POST);
+$_SESSION['message'] = 'Admin user created successfully';
+$_SESSION['type']='success';
+header('location:' . BASE_URL . '/admin/users/index.php');
+exit() ;
+}
+else {
+    $_POST['role'] = 'Author';
+    $user_id = create($table, $_POST);
+    $user = SelectOne($table, ['id' => $user_id]);
+    loginUser($user);
+}
     } else {
         $username = isset($_POST['username']) ? $_POST['username'] : '';
         $email = isset($_POST['email']) ? $_POST['email'] : '';
@@ -101,7 +101,7 @@ if (isset($_GET['id'])) {
     $user = selectOne($table, ['id' => $_GET['id']]);
     $id = $user['id'];
     $role = $user['role'] === 'Admin' ? true : false;
-    $username = $user['username'];  
+    $username = $user['username'];
     $email = $user['email'];
 }
 
@@ -130,11 +130,10 @@ if (isset($_POST['update-user'])) {
 
         $_POST['role'] = isset($_POST['role']) ? "Admin" : "Author";
 
-
         $count = update($table, $id, $_POST);
         $_SESSION['message'] = 'Admin user updated successfully';
         $_SESSION['type'] = 'success';
-       header('location:' . BASE_URL . '/admin/users/index.php');
+        header('location:' . BASE_URL . '/admin/users/index.php');
         exit();
     } else {
         $username = isset($_POST['username']) ? $_POST['username'] : '';
@@ -143,5 +142,3 @@ if (isset($_POST['update-user'])) {
         $passwordConf = isset($_POST['repeat-password']) ? $_POST['repeat-password'] : '';
     }
 }
-
-
