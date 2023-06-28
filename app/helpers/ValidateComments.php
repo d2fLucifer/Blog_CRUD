a43398b14c353462c6167cd709857b49eecb8d0e
d2fLucifer <d2f.working@gmail.com>
2023-06-29 00:22:30 +0700

    update comments

--- app/helpers/ValidateComments.php
+++ app/helpers/ValidateComments.php
@@ -1,19 +1,16 @@
 <?php
-function validateComments($comments)
+
+function validateComments($commentData)
 {
-    $errors = array();
-    if (empty($comments['post_id'])) {
-        array_push($errors, 'Post ID is required');
-    }
-    if (empty($comments['user_id'])) {
-        array_push($errors, 'User ID is required');
-    }
-    if (empty($comments['body'])) {
-        array_push($errors, 'Comment body is required');
+    $errors = [];
+
+    // Validate the comment body
+    if (empty($commentData['comment-body'])) {
+        $errors[] = 'Comment body is required';
     }
 
-    // You can add additional validation rules for the comments if needed
-    
+    // Add more validation rules if needed
+
     return $errors;
 }
 ?>
